aa2635e6a976297cb69c0e93fc16775321f0615c
santhoshpodugu <santhoshsanthu1973@gmail.com>
2024-02-25 00:22:49 +0530

    doc formatting

--- scripts.js
+++ scripts.js
@@ -1,21 +1,20 @@
-const hamburger = document.querySelector('.hamburger-menu');
-const navMenu = document.querySelector('.nav-menu');
-const navLink = document.querySelectorAll('.nav-link');
+const hamburger = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
 navLink.forEach((link) => {
-    link.addEventListener('click', () => {
-        hamburger.classList.remove('active');
-        navMenu.classList.remove('active');
-    });
-    
-})
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
 
 var typed = new Typed("#element", {
-    strings: ["<b>UPSC</b> Aspirant", "Tech Enthusiast", "Web Developer."],
-    typeSpeed: 50,
-  });
+  strings: ["<b>UPSC</b> Aspirant", "Tech Enthusiast", "Web Developer."],
+  typeSpeed: 50,
+});
